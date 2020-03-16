a3a601e7ac973907d5cc43fb224141c07cfacaa6
Ian Hooper <ianhooper8@gmail.com>
2020-03-15 23:33:50 -0400

    finished local storage for all time slots

--- script.js
+++ script.js
@@ -14,4 +14,107 @@ $(document).ready(function() {
 
         $('#9am').val(storedTodos)
     }
+
+    getTodoOne()
+    $('.saveBtn').on('click', function(event) {
+        event.preventDefault()
+        var todo = $('#10am').val()
+        localStorage.setItem('todo1', todo)
+        getTodoOne()
+    })
+    function getTodoOne() {
+        var storedTodos = localStorage.getItem('todo1')
+
+        $('#10am').val(storedTodos)
+    }
+
+    getTodoTwo()
+    $('.saveBtn').on('click', function(event) {
+        event.preventDefault()
+        var todo = $('#11am').val()
+        localStorage.setItem('todo2', todo)
+        getTodoTwo()
+    })
+    function getTodoTwo() {
+        var storedTodos = localStorage.getItem('todo1')
+
+        $('#11am').val(storedTodos)
+    }
+
+    getTodoThree()
+    $('.saveBtn').on('click', function(event) {
+        event.preventDefault()
+        var todo = $('#12pm').val()
+        localStorage.setItem('todo3', todo)
+        getTodoThree()
+    })
+    function getTodoThree() {
+        var storedTodos = localStorage.getItem('todo3')
+
+        $('#12pm').val(storedTodos)
+    }
+
+    getTodoFour()
+    $('.saveBtn').on('click', function(event) {
+        event.preventDefault()
+        var todo = $('#1pm').val()
+        localStorage.setItem('todo4', todo)
+        getTodoFour()
+    })
+    function getTodoFour() {
+        var storedTodos = localStorage.getItem('todo4')
+
+        $('#1pm').val(storedTodos)
+    }
+
+    getTodoFive()
+    $('.saveBtn').on('click', function(event) {
+        event.preventDefault()
+        var todo = $('#2pm').val()
+        localStorage.setItem('todo5', todo)
+        getTodoFive()
+    })
+    function getTodoFive() {
+        var storedTodos = localStorage.getItem('todo5')
+
+        $('#2pm').val(storedTodos)
+    }
+    getTodoSix()
+    $('.saveBtn').on('click', function(event) {
+        event.preventDefault()
+        var todo = $('#3pm').val()
+        localStorage.setItem('todo6', todo)
+        getTodoSix()
+    })
+    function getTodoSix() {
+        var storedTodos = localStorage.getItem('todo6')
+
+        $('#3pm').val(storedTodos)
+    }
+
+    getTodoSeven()
+    $('.saveBtn').on('click', function(event) {
+        event.preventDefault()
+        var todo = $('#4pm').val()
+        localStorage.setItem('todo7', todo)
+        getTodoSeven()
+    })
+    function getTodoSeven() {
+        var storedTodos = localStorage.getItem('todo7')
+
+        $('#4pm').val(storedTodos)
+    }
+
+    getTodoEight()
+    $('.saveBtn').on('click', function(event) {
+        event.preventDefault()
+        var todo = $('#5pm').val()
+        localStorage.setItem('todo8', todo)
+        getTodoEight()
+    })
+    function getTodoEight() {
+        var storedTodos = localStorage.getItem('todo8')
+
+        $('#5pm').val(storedTodos)
+    }
 })
